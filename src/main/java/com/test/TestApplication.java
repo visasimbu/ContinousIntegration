9911ae1cfbc4b129f1a7bab6cdbd9f13d61fc474
Simbu <learn90simbu@outlook.com>
2022-04-17 14:23:22 +0530

    Initial Draft

--- src/main/java/com/test/TestApplication.java
+++ src/main/java/com/test/TestApplication.java
@@ -6,7 +6,8 @@ import com.intuit.karate.Runner;
 public class TestApplication {
     public static void main(String[] args) {
 
-        Results results = Runner.path("classpath:features")
+        Results results = Runner.path("file:E:\\Simbu\\POC\\features\\NewdemoAPI.feature")
+//        Results results = Runner.path("classpath:features")
                 .tags("@Integration")
                 .parallel(2);
         System.exit(results.getFailCount());
